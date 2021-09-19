89e6fab5920f3cfe93e52c26e06d3f13b276f208
Jonathan Lanson <jlanson@uwaterloo.ca>
2021-09-19 05:00:22 -0400

    PointEntity

---
+++ UbiGame_Blank/Source/Game/GameEntities/PointEntity.cpp
@@ -0,0 +1,47 @@
+#include "PointEntity.h"
+
+
+#include "GameEngine/GameEngineMain.h"
+#include "SFML/Graphics/Color.hpp"
+
+#include <iostream>
+
+int previous=0;
+using namespace Game;
+
+PointEntity::PointEntity()
+{
+    m_renderComponent = AddComponent<GameEngine::TextRenderComponent>();
+    m_renderComponent->SetFont("comici.ttf");
+    m_renderComponent->SetCharacterSizePixels(30);
+    m_renderComponent->SetColor(sf::Color(255,255,255));
+	m_renderComponent->SetString("Points: 0");
+	m_renderComponent->SetZLevel(3);
+    SetPos(sf::Vector2f{450.f,950.f});
+}
+
+PointEntity::~PointEntity()
+{
+}
+
+void PointEntity::OnAddToWorld()
+{
+    Entity::OnAddToWorld();
+}
+
+void PointEntity::OnRemoveFromWorld()
+{
+    Entity::OnRemoveFromWorld();
+}
+
+
+void PointEntity::Update()
+{
+    Entity::Update();
+    if(PointManager::GetInstance()->GetGoalBlobs() > previous){
+        points = PointManager::GetInstance()->GetGoalBlobs();
+    }
+
+    m_renderComponent->SetString("Points: " + std::to_string(points));
+}
+
