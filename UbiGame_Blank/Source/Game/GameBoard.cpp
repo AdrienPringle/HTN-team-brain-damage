39321fc12b0bea0f900bd30504cb8e0180631b1e
Jonathan Lanson <jlanson@uwaterloo.ca>
2021-09-19 07:36:08 -0400

    yes

--- UbiGame_Blank/Source/Game/GameBoard.cpp
+++ UbiGame_Blank/Source/Game/GameBoard.cpp
@@ -4,8 +4,6 @@
 #include "Game/Util/MenuController.h"
 #include "Game/Util/WallManager.h"
 #include "Game/Util/PointManager.h"
-#include "Game/GameEntities/TimerEntity.h"
-#include "Game/GameEntities/LevelEntity.h"
 
 #include<iostream>
 
@@ -13,16 +11,6 @@ using namespace Game;
 
 GameBoard::GameBoard()
 {
-    TimerEntity* timer = new TimerEntity();
-    timer->setTime(1);
-    GameEngine::GameEngineMain::GetInstance()->AddEntity(timer);
-
-    
-
-    LevelEntity* level = new LevelEntity();
-    level->setTime(1);
-    level->setFrenquency(5);
-    GameEngine::GameEngineMain::GetInstance()->AddEntity(level);
 }
 
 
